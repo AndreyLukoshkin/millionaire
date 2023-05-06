06297f0fe19dcbe3402833bfc00cdc1fc072756b
Andrey Lukoshkin <lukoshkin.andrey@gmail.com>
2023-05-06 08:29:10 +0300

    another test for onrender

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,9 +2,9 @@ import styles from './StylesModules/QuestionsAndScores.module.css'
 
 const Question = ({ currentQuestion }) => {
   return (
-    <div>
+    <>
       <h1 className={styles.questionText}>{currentQuestion?.question}</h1>
-    </div>
+    </>
   )
 }
 
